ab9eb2e95d9ea7993e33e5c94dd525ab57beea12
dbsqwerty123 <dbsqwerty123@gmail.com>
2020-05-16 22:28:33 +0800

    w

--- commands/list.js
+++ commands/list.js
@@ -1,5 +1,5 @@
 const dbOps = require('../util/dbOps.js')
-const FeedSelector = require('../structs/testing.js')
+const FeedSelector = require('../structs/FeedSelector.js')
 const log = require('../util/logger.js')
 const MenuUtils = require('../structs/MenuUtils.js')
 
@@ -20,7 +20,7 @@ module.exports = async (bot, message, command, role) => {
   try {
     const guildRss = await dbOps.guildRss.get(message.guild.id)
     const feedSelector = new FeedSelector(message, feedSelectorFn, { command: command }, guildRss)
-    const data = await new MenuUtils.MenuSeries(message, [feedSelector]).start()
+    const data = new MenuUtils.MenuSeries(message, [feedSelector]).start()
 
 
   } catch (err) {
